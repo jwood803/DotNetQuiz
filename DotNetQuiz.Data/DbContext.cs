ee737b0e478cd58f8c7ba048850cc54099434fe4
Jon <jwood803@users.noreply.github.com>
2013-10-26 16:56:39 -0400

    Adding stubs for the DbContext

---
+++ DotNetQuiz.Data/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using ServiceStack.OrmLite;
+using System.Configuration;
+namespace DotNetQuiz.Data
+{
+    public class DbContext
+    {
+        private readonly IDbConnectionFactory connection;
+
+        public DbContext()
+        {
+            connection = new OrmLiteConnectionFactory(ConfigurationManager.AppSettings["SqliteFile"], false, SqliteDialect.Provider);
+        }
+
+        public DbContext(IDbConnectionFactory connection)
+        {
+            this.connection = connection;
+        }
+    }
+}
